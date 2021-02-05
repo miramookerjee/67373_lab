c369fc5c02f2da533dc491457c5894e5e63c6c4c
miramookerjee8000@gmail.com <miramookerjee8000@gmail.com>
2021-02-04 20:14:15 -0500

    added validation matching

--- test/models/child_test.rb
+++ test/models/child_test.rb
@@ -7,6 +7,9 @@ class ChildTest < ActiveSupport::TestCase
   
   # TODO: Validation macros
 
+   should validate_presence_of(:first_name)
+   should validate_presence_of(:last_name)
+
   # TODO: Context testing
 
 end

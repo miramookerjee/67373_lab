1f51936540487b7a1138bfb9b75cf46b3c00b003
miramookerjee8000@gmail.com <miramookerjee8000@gmail.com>
2021-02-04 20:27:47 -0500

    implemented child model to pass the tests

--- app/models/child.rb
+++ app/models/child.rb
@@ -1,3 +1,13 @@
 class Child < ApplicationRecord
+	has_many :chores
+	has_many :tasks, through: :chores
+	validates_presence_of :first_name, blank: :false
+	validates_presence_of :last_name, blank: :false
     
+    def name
+		return first_name + " " + last_name
+	end
+
+	scope :alphabetical, -> {order('last_name', 'first_name')}
+    scope :active, -> { where(active: true) }
 end
